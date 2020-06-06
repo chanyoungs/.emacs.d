65d6b0987d2b55f5a53ed817548cf829122a1419
chanyoungs@gmail.com <chanyoungs@gmail.com>
2020-06-06 15:08:29 +0100

    Added "GIT_SSH" env: see myinit.org -> magit

---
+++ init.el
@@ -0,0 +1,32 @@
+(setenv "GIT_ASKPASS" "git-gui--askpass")
+(setq default-directory "C:/Users/chany/Documents/Programming/")
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/")
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auctex js2-mode browse-url-dwim iedit expand-region beacon impatient-mode simple-httpd atom-one-dark-theme cherry-blossom-theme ample-theme monokai-theme elpy smartparens node\.js org flycheck el2org zenburn-theme which-key use-package try counsel color-theme auto-org-md auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'narrow-to-region 'disabled nil)
